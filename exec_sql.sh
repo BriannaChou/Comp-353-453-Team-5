a2814558b8ed6ccff4cd81829d031ebf11424720
jkotheimer <jkotheimer@luc.edu>
2020-04-02 13:08:03 -0500

    Changed config file to be hidden

--- exec_sql.sh
+++ exec_sql.sh
@@ -1,10 +1,10 @@
 [ ! $1 ] && >&2 echo "Please provide a .sql file as a parameter" && exit 1
-if [ ! -f mysql.cfg ]; then
+if [ ! -f .mysql.cfg ]; then
 	read -p "MySQL username: " username
 	read -sp "MySQL password: " password
 	echo ""
-	echo "username=\"$username\";password=\"$password\"" > mysql.cfg
-	chmod 600 mysql.cfg
+	echo "username=\"$username\";password=\"$password\"" > .mysql.cfg
+	chmod 600 .mysql.cfg
 fi
-source mysql.cfg
+source .mysql.cfg
 mysql -u"$username" -p"$password" < $1 
