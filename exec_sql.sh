868c9b6fb0c7f01b0bc14095a5a8b970ec441c02
jkotheimer <jkotheimer@luc.edu>
2020-04-02 12:58:26 -0500

    Created script to quickly execute SQL queries without having to go to PHPMyAdmin

---
+++ exec_sql.sh
@@ -0,0 +1,10 @@
+[ ! $1 ] && >&2 echo "Please provide a .sql file as a parameter" && exit 1
+if [ ! -f mysql.cfg ]; then
+	read -p "MySQL username: " username
+	read -sp "MySQL password: " password
+	echo ""
+	echo "username=\"$username\";password=\"$password\"" > mysql.cfg
+	chmod 600 mysql.cfg
+fi
+source mysql.cfg
+mysql -u"$username" -p"$password" < $1 
