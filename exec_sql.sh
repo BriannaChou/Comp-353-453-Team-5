6c5edf221692cb8dd50e889491ce3786c746e953
jkotheimer <jkotheimer@luc.edu>
2020-04-02 14:11:59 -0500

    Added absolute path verification (see comment)

--- exec_sql.sh
+++ exec_sql.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
 [ ! $1 ] && >&2 echo "Please provide a .sql file as a parameter" && exit 1
-if [ ! -f .mysql.cfg ]; then
+
+# In case if this script is being executed from some different directory, 
+# take note of the absolute path of the provided SQL file then cd into 
+# the directory in which this script is held
+SQL_PATH="$(pwd)/$1"
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ ! -f flask_app/.mysql.cfg ]; then
 	read -p "MySQL username: " username
 	read -sp "MySQL password: " password
 	echo ""
-	echo "username=\"$username\";password=\"$password\"" > .mysql.cfg
-	chmod 600 .mysql.cfg
+	echo "username=\"$username\";password=\"$password\"" > flask_app/.mysql.cfg
+	chmod 600 flask_app/.mysql.cfg
 fi
-source .mysql.cfg
-mysql -u"$username" -p"$password" < $1 
+
+source flask_app/.mysql.cfg
+mysql -u"$username" -p"$password" < $SQL_PATH 
