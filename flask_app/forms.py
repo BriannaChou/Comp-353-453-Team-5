6fd9bcc960b478b7bba2b9f316ad74fd01d9f3c1
jkotheimer <jkotheimer@luc.edu>
2020-04-18 20:48:06 -0500

    Merged updated forms

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -2,21 +2,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
-
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+	name = StringField('Name', validators=[DataRequired(), Length(min=1, max=32)])
+	
+	phone = StringField('Phone', validators=[DataRequired(), Length(min=1, max=16)])
+	
+	email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=320)])
+	
+	password = PasswordField('Password', validators=[DataRequired()])
+	
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password'), Length(min=1, max=60)])
+	
+	submit = SubmitField('Sign Up')
+	
+	def validate_email(self, email):
+		user = User.query.filter_by(Email=email.data).first()
+		if user:
+			raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+	email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=320)])
+	
+	password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=60)])
+	
+	submit = SubmitField('Login')
