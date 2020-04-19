200711eb53b5c9f5eb89d16bb0e7a1e62f268979
Jean-Luc Putter <jean-lucputter@Jean-Lucs-MacBook-Pro.local>
2020-04-19 15:59:08 -0500

    finally running locally for me

--- chat_app/app.py
+++ chat_app/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from sql_config import username, password
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -11,7 +10,7 @@ bcrypt = Bcrypt()
 # Flask and SQL setup 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + username + ':' + password + '@localhost/ChatApp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://jputter:jputter@localhost:8889/ChatApp'
 
 # Initialize plugins
 db.init_app(app)
